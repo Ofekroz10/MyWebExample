51dcbd6a996aa2c697622607e1a1813233f92d8c
Ofek rozenkrantz <57916083+Ofekroz10@users.noreply.github.com>
2020-04-02 04:26:13 +0300

    Add files via upload

---
+++ Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            int grade = 0;
+
+            for(int i=0; i<10;i++)
+            {
+                int temp = i + i;
+                if (temp == Program.Add(i, i))
+                    grade += 10;
+            }
+
+            Console.WriteLine("Your grade is: "+grade);
+        }
+    }
+}
